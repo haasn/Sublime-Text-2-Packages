9ada2b2231ed74a35d1d29703491159a4c5bd3d9
Josh Ourisman <josh@joshourisman.com>
2011-10-12 17:07:38 -0400

    simplified flake8 plugin

--- Python/flake8.py
+++ Python/flake8.py
@@ -1,4 +1,3 @@
-import os
 import sublime
 import sublime_plugin
 
@@ -6,7 +5,7 @@ import sublime_plugin
 class Pep8CheckCommand(sublime_plugin.EventListener):
     def on_post_save(self, view):
         if view.file_name().endswith('.py'):
-            folder_name, file_name = os.path.split(view.file_name())
+            file_name = view.file_name()
             view.window().run_command('exec', {'cmd': ['flake8',
-                file_name], 'working_dir': folder_name})
+                file_name]})
             sublime.status_message("pep8 " + file_name)
